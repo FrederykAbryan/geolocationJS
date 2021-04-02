fabbbc231e326d374650b6774aecb6e07e893780
FrederykAbryan <fabryan03@gmail.com>
2021-04-02 21:50:48 +0800

    commit marker2

--- script.js
+++ script.js
@@ -23,6 +23,8 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map;
+
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
@@ -32,7 +34,7 @@ if (navigator.geolocation)
 
       const coords = [latitude, longitude];
 
-      const map = L.map('map').setView(coords, 13);
+      map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution:
@@ -40,12 +42,29 @@ if (navigator.geolocation)
       }).addTo(map);
 
       map.on('click', function (mapEvent) {
-        const { lat, lng } = mapEvent.latlng;
-
-        L.marker([lat, lng]).addTo(map).bindPopup('Custom pop coy').openPopup();
+        form.classList.remove('hidden');
+        inputDistance.focus();
       });
     },
     function () {
       alert('could not get your position');
     }
   );
+
+form.addEventListener('submit', function () {
+  const { lat, lng } = mapEvent.latlng;
+
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
